90d07aee12488ade71ff1648431b921eb9f407da
arthur-carneiro <arthurlima.carneiro@gmail.com>
2020-09-05 12:25:59 -0300

    commit da rodada 8

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,7 +5,7 @@ from mysite.database import Main
 from mysite.database.return_games import return_games
 from mysite.database.return_class_cmplt import return_class_cmplt
 
-RODADA = "Rodada 7"
+RODADA = "Rodada 8"
 MES = "Setembro/2020"
 
 # Create your views here.
